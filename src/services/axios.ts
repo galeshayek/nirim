93555c7524b5fc8399febfb340250a39d86d87cb
galeshayek <galeshayek15@gmail.com>
2024-05-23 20:18:43 +0300

    .

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const baseUrl = 'http://nirim-site-env.eba-5jpqpx6x.us-east-2.elasticbeanstalk.com/api/v1'
+export const baseUrl = 'https://txe8s9sm4h.execute-api.us-east-2.amazonaws.com/prod/api/v1'
 export const localUrl = 'http://localhost:8080/api/v1'
 export async function getUsers() {
     const response = await axios.get(`${baseUrl}/users/all`)
