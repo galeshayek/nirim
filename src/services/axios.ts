c8934e7be0c7f524a8d1d3b9024c7b6b80620c38
galeshayek <galeshayek15@gmail.com>
2024-08-08 20:41:07 -0400

    changed api adress

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const baseUrl = 'https://txe8s9sm4h.execute-api.us-east-2.amazonaws.com/prod/api/v1'
+export const baseUrl = 'https://nirimserver.space/api/v1'
 export const localUrl = 'http://localhost:8080/api/v1'
 export async function getUsers() {
     const response = await axios.get(`${baseUrl}/users/all`)
